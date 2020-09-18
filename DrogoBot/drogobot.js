ba2a15322af9f19cd8b8f528adf0174a771bca77
Drogoton <drogoton@kouvradio.com>
2020-09-17 21:32:45 -0700

    9-17-20-pog

--- DrogoBot/drogobot.js
+++ DrogoBot/drogobot.js
@@ -131,7 +131,6 @@ client.on('message' , async message => {
         .setFooter('made by un boxing man yt', 'http://unpix.nwpixs.com/logo.png')
 
 	   message.channel.send(Embed)
-<<<<<<< HEAD
 	   console.log(`1${name} 2${link} 3${logo} 4${idk}`)
   } else if(command.startsWith(`${prefix}welcome`)){ 
     message.delete()
@@ -139,16 +138,6 @@ client.on('message' , async message => {
   } else if(command.startsWith(`${prefix}`)){ 
     message.channel.send("DrogoBot help menu \n + is the help command you already ran, dummy! \n +welcome Welcomes people. \n +say Tells the bot to say something in that channel. \n +gif Searches for gifs depending on what follows after the command.")
   } 
-=======
-     console.log(`1${name} 2${link} 3${logo} 4${idk}`)
-     
-	} else if (command.startsWith(`pogalljoin`)) {
-    voicechannel.join()
-    .then(connection => console.log('Connected!'))
-    .catch(console.error);
-}
-
->>>>>>> 11358596ccf0a4e182a8bc442ddaeee887d9a493
 
 })
 

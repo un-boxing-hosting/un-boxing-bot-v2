27fa3ab5c22d8f9acd25536bea26c0492aada0fb
Drogoton <drogoton@kouvradio.com>
2020-10-05 06:40:34 -0700

    10-5-202

--- PB/PB.js
+++ PB/PB.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const { prefix, token, GIPHYtoken, channelID } =require(`./PB-config.json`);
 const client = new Discord.Client();
+//const bot = new Discord.Client();
+const gac = require('giphy-js-sdk-core');
+const Giphy = gac(GIPHYtoken);
 
 client.on("ready", () => {
 	console.log('Ready!');
@@ -9,6 +12,7 @@ client.on("ready", () => {
   .catch(console.error);
   console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
   });
+
   client.on("guildCreate", guild => {
 	// This event triggers when the bot joins a guild.
 	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
@@ -28,6 +32,7 @@ client.once('reconnecting', () => {
 client.once('disconnect', () => {
 	console.log('Disconnect!');
 });
+
 client.on('message', async dmmessage => {
     
     if (!dmmessage.channel.type === 'dm') return;
@@ -35,7 +40,7 @@ client.on('message', async dmmessage => {
         if (dmmessage.author.bot) return
         const dms = dmmessage.content;
         const dmauthor = dmmessage.author.tag;
-        //console.log(`message ${dms} sent by ${dmauthor} in dm`)
+        console.log(`message ${dms} sent by ${dmauthor} in dm`)
         const channel = client.channels.cache.get(`${channelID}`)
         const dmEmbed = new Discord.MessageEmbed()
         
@@ -44,7 +49,7 @@ client.on('message', async dmmessage => {
       .setURL('http://dro.unboxingman.com')
       .setAuthor('PotatoMC Bot', 'http://play.unboxingman.com/dro/PotatoMC.png', 'http://dro.unboxingman.com')
       .setDescription(` Received: ${dms}`)
-      .setThumbnail('http://play.unboxingman.com/dro/PotatoMC.png')
+      .setThumbnail('http://play.unboxingman.com/dro/DrogoLogo.png')
       .addFields(
           //{ name: 'new dm message', value: `${dms}` }, 
           { name:`by ${dmauthor}`,  value: `.`},
@@ -56,17 +61,40 @@ client.on('message', async dmmessage => {
 	   console.log(`message ${dms} sent by ${dmmessage.author.tag} in dm`)
 	}
 });
+
 client.on('message' , async message => {
-	if (message.author.bot) return;
+	//if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = message.content.toLocaleLowerCase();
   const mention = message.mentions.users.first();
   const Member = message.member;
-    const voicechannel = Member.voice.channel;
-
-    if (command.startsWith(`${prefix}ping`)) { 
+	const voicechannel = Member.voice.channel;
+    
+  if (command.startsWith(`${prefix}ping`)) { 
         const m = await message.channel.send("Ping?");
-         m.edit(`Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
-    }
+         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
+
+  } else if (command.startsWith(`${prefix}say`)){ 
+      if (message.member.hasPermission( 'MANAGE_MESSAGES', { checkAdmin:true, checkOwner: true })){
+        var messagesay = message.content;
+        var sayMessage= messagesay.substr('4');
+        message.delete().catch(O_o=>{});  
+        message.channel.send(sayMessage);
+        console.log(sayMessage ,"sent by" ,message.author.tag,"in channel ", message.channel.name, "in guild", message.guild.name)
+      }  
+  } else if (command.startsWith(`${prefix}dm`)){
+		const mentionmessage = message.content.slice(5);
+		if(mention == null) { return; }
+		message.delete();
+		mentionmessage.slice(mention);
+		mention.send(mentionmessage);
+    message.channel.send("done!")
+    
+  } else if(command.startsWith(`${prefix}help`)){ 
+   message.channel.send("PotatoMC Bot help menu \n +help is the help command you already ran, dummy! \n +welcome Welcomes people. \n +say Tells the bot to say something in that channel. \n +gif Searches for gifs depending on what follows after the command.")
+  }
+
 })
+
+client.login(token)

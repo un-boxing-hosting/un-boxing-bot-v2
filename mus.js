833553a3149efb43affb51a306d587d4002a0b8b
un-boxing-man <unboxingman@hotmail.com>
2020-10-09 18:14:05 -0700

    help me i ned to drenk blech

--- mus.js
+++ mus.js
@@ -527,6 +527,18 @@ client.on('message', async message => {
 		.then(() => console.log(`unbanned ${buser} from ${message.guild.name} for ${reson}`))
 		.catch(console.error);
 	message.channel.send(`unbanned ${buser} for ${reson}`)
+ 	} else if (message.content.startsWith(`${prefix}pix`)){
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+		}
+	
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+		});
+	
+		// send the entire array of strings as a message
+		// by default, discord.js will `.join()` the array with `\n`
+		message.channel.send(avatarList);
     } else {
 	message.channel.send('You need to enter a valid command!\n try u!help')
 	}
